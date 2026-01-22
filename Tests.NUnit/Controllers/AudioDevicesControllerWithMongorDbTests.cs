7329db9cdf4bb26e66a420311a9fa0314e4caddf
danziger-e <eduard.danziger@t-online.de>
2026-01-23 00:33:34 +0100

    AudioDevicesControllerWithMongorDbTests verifies controller behavior with a real MongoDB backend.

---
+++ Tests.NUnit/Controllers/AudioDevicesControllerWithMongorDbTests.cs
@@ -0,0 +1,80 @@
+using DeviceControllerLib.Controllers;
+using DeviceControllerLib.Models.RestApi;
+using DeviceControllerLib.Services;
+using DeviceControllerLib.Settings;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+
+namespace Tests.NUnit.Controllers;
+
+[TestFixture]
+[Category("MongoDB")]
+public class AudioDevicesControllerWithMongorDbTests
+{
+    private sealed class FakeCryptService : ICryptService
+    {
+        public string TryDecryptOrReturnOriginal(string originalText, string passphrase) => originalText;
+
+        public string ComputeChecksum(string str) => "7d1a6195";
+    }
+
+    private static EntireDeviceMessage CreateValidDevice(string pnpId = "pnp-3", string hostName = "HOST03",
+        string name = "Device")
+        => new()
+        {
+            PnpId = pnpId,
+            HostName = hostName,
+            Name = name,
+            OperationSystemName = "Windows",
+            FlowType = DeviceFlowType.Render,
+            RenderVolume = 100,
+            CaptureVolume = 200,
+            UpdateDate = DateTime.UtcNow,
+            DeviceMessageType = DeviceMessageType.Confirmed
+        };
+
+    private static MongoDbAudioDeviceStorage CreateStorage()
+    {
+        var settings = new MongoDbSettings
+        {
+            ConnectionStringAnonymous = "mongodb+srv://cluster0.mdlxo.mongodb.net/?retryWrites=true&w=majority&connectTimeoutMS=30000&socketTimeoutMS=30000&serverSelectionTimeoutMS=30000",
+            DatabaseName = "audio_device_repository",
+            DatabaseUser = "ed",
+            DatabasePassword = "Qwer.1234"
+        };
+
+        var options = Options.Create(settings);
+        return new MongoDbAudioDeviceStorage(options, new FakeCryptService(), NullLogger<MongoDbAudioDeviceStorage>.Instance);
+    }
+
+    [Test]
+    public void Add_Check_Remove()
+    {
+        var storage = CreateStorage();
+        var cryptService = new FakeCryptService();
+        var controller = new AudioDevicesController(storage, cryptService);
+
+        var allLengthBeforeAdd = storage.GetAll().ToArray().Length;
+
+        var device = CreateValidDevice(hostName: "MyPc");
+        var result = controller.Add(device);
+
+        Assert.That(result, Is.InstanceOf<CreatedAtActionResult>());
+
+        var created = (CreatedAtActionResult)result;
+        Assert.That(created.ActionName, Is.EqualTo(nameof(AudioDevicesController.GetByKey)));
+
+        var all = storage.GetAll().ToArray();
+        Assert.That(all.Length, Is.EqualTo(allLengthBeforeAdd + 1));
+        Assert.That(all[allLengthBeforeAdd].PnpId, Is.EqualTo(device.PnpId));
+        Assert.That(all[allLengthBeforeAdd].HostName, Is.EqualTo(cryptService.ComputeChecksum("MyPc")));
+
+        result = controller.Remove(device.PnpId, device.HostName);
+
+        Assert.That(result, Is.InstanceOf<NoContentResult>());
+        Assert.That(storage.GetAll().ToArray().Length, Is.EqualTo(allLengthBeforeAdd));
+
+    }
+}
